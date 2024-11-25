64520d600b5ba8f9fad882522af79d5e886fc8ba
Akk-121 <mihail.kisliakow@yandex.ru>
2024-11-25 12:15:19 +0300

    Подготовка БД

--- ntu_project/settings.py
+++ ntu_project/settings.py
@@ -105,14 +105,17 @@ WSGI_APPLICATION = 'ntu_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        "OPTIONS": {
-                    "service": "my_service",
-                    "passfile": ".my_pgpass",
-                },
+        # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgresbase',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
